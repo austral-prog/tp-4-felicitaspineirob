00833f8cf0f8b8e50cdf4fd9b0b4465a20368f33
felicitaspineirob <162723113+felicitaspineirob@users.noreply.github.com>
2024-04-18 21:31:08 -0300

    Update line.py

--- line.py
+++ line.py
@@ -10,7 +10,7 @@ def line():
 	print (f"El coeficiente X1 de su ecuación de la recta es:{X1}")
 	print (f"El coeficiente X2 de su ecuación de la recta es:{X2}\n")
 	print (f"Para la siguiente ecuacion: \n \tY= {A}X + {B}\n")
-	print (f"Dados los siguientes puntos:")
+	print (f"Dados los siguientes puntos: ")
 	Y1= A*X1 + B
 	Y2= A*X2 + B
 	print (f"\t P1 ({X1}, {Y1})")
